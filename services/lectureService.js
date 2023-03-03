d9d19ed995670cc46c713ead6f8be329549ab68b
이홍열 <zeler1004@gmail.com>
2023-03-03 20:50:34 +0900

    [ADD] 강의 상세페이지 API 기능 구현

--- services/lectureService.js
+++ services/lectureService.js
@@ -4,4 +4,29 @@ const getLectures = async (category) => {
   return lectureDao.getLectures(category);
 };
 
-module.exports = { getLectures };
+const getLectureDetail = async (lectureId) => {
+  let lectureDetail = {};
+
+  const lectureData = await lectureDao.getLecture(lectureId);
+  const detailImages = await lectureDao.getDetailImage(lectureId);
+  const curriculums = await lectureDao.getCurriculum(lectureId);
+  const videos = await lectureDao.getVideo(lectureId);
+
+  lectureDetail = lectureData[0];
+  lectureData[0].detailImages = detailImages;
+  lectureDetail.curriculums = curriculums[0];
+  curriculums[0].videos = videos;
+
+  const curriculumsWithVideos = curriculums.map((curriculum) => {
+    const videosForCurriculum = videos.filter((video) => {
+      return video.curriculumId === curriculum.curriculumId;
+    });
+    curriculum.videos = videosForCurriculum;
+    return curriculum;
+  });
+  lectureDetail.curriculums = curriculumsWithVideos;
+
+  return lectureDetail;
+};
+
+module.exports = { getLectures, getLectureDetail };
