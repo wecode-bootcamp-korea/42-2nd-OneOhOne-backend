e0f47bf0172becc4f9907aa03473c07e1aca105b
wonkyung Choi <wonkyungchoi@wonkyungui-MacBookPro.local>
2023-03-08 20:08:20 +0900

    fix database tables/column 수정

--- db/migrations/20230227083950_lectures.sql
+++ db/migrations/20230227083950_lectures.sql
@@ -6,14 +6,14 @@ CREATE TABLE lectures(
   description VARCHAR(3000) NULL,
   main_image_url VARCHAR(500) NULL,
   creator_id INT NOT NULL,
-  category_id INT NOT NULL,
-  status_id INT NOT NULL,
+  category_name VARCHAR(100) NOT NULL,
+  status_name VARCHAR(100) NULL,
+  user_id int NOT NULL,
   create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   update_at TIMESTAMP NULL ON UPDATE  CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   CONSTRAINT lectures_creator_id_fkey FOREIGN KEY (creator_id)  REFERENCES creators (id),
-  CONSTRAINT lectures_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories (id),
-  CONSTRAINT lectures_lecture_status_id_fkey FOREIGN KEY (status_id) REFERENCES lecture_status (id)
+  CONSTRAINT lectures_users_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 -- migrate:down
--- db/migrations/20230227092246_detail_images.sql
+++ db/migrations/20230227092246_detail_images.sql
@@ -2,7 +2,7 @@
 CREATE TABLE detail_images(
   id INT NOT NULL AUTO_INCREMENT,
   lecture_id INT NOT NULL,
-  sequence INT NOT NULL,
+  sequence INT NULL,
   image_url VARCHAR(500) NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT detail_images_lecture_id_fkey FOREIGN KEY (lecture_id) REFERENCES lectures(id)
